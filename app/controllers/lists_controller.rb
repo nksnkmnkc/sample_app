46b2558ca12920200b4f9135da8f2be92a595c3b
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-10-10 01:55:54 +0000

    [Finish]4-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,11 +8,13 @@ class ListsController < ApplicationController
    list.save
    redirect_to'/top'
   end
-  
+
   def index
+   @lists = List.all
   end
 
   def show
+   @list = List.find(params[:id])
   end
 
   def edit
