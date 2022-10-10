0113556f51d299a3ed1ed140201c565f0efb8298
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-10-10 02:22:55 +0000

    [Finish]4-4

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
 
-  get 'lists/:id' => 'lists#show'
+  get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/edit'
   get '/top' => 'homes#top'
 
